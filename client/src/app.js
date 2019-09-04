0eee330c9d8908c2e43fa000c48e230aa10a8797
duke79 <pulkitsingh01@gmail.com>
2019-09-04 08:18:25 +0530

    client binds with server

--- client/src/app.js
+++ client/src/app.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import {observer} from 'mobx-react'
+import store from './store'
 
 class App extends React.Component {
     constructor(props) {
@@ -12,7 +13,8 @@ class App extends React.Component {
 
         this.handle_key = this.handle_key.bind(this);
         this.handle_value = this.handle_value.bind(this);
-        this.handle_submit = this.handle_submit.bind(this);
+        this.on_set = this.on_set.bind(this);
+        this.on_get = this.on_get.bind(this);
     }
 
     handle_key(e) {
@@ -27,22 +29,44 @@ class App extends React.Component {
         })
     }
 
-    handle_submit(e) {
+    on_set(e) {
+        store.set_key(this.state.key, this.state.value);
+    }
+
+    on_get(e) {
+        store.get_key(this.state.key);
     }
 
     render() {
+        let store_value = store.pairs.get(this.state.key);
+        if (null === store_value) {
+            store_value = "Key not found!"
+        }
+
         return (
-            <form onSubmit={this.handle_submit}>
-                <label>
-                    Key:
-                    <input type="text" value={this.state.key} onChange={this.handle_key}/>
-                </label>
-                <label>
-                    Value:
-                    <input type="text" value={this.state.value} onChange={this.handle_value}/>
-                </label>
-                <input type="submit" value="Submit"/>
-            </form>
+            <div>
+                <div>
+                    <label>
+                        Key:
+                        <input type="text" value={this.state.key} onChange={this.handle_key}/>
+                    </label>
+                    <label>
+                        Value:
+                        <input type="text" value={this.state.value} onChange={this.handle_value}/>
+                    </label>
+                    <input type="submit" value="set" onClick={this.on_set}/>
+                </div>
+
+                <div>
+                    <label>
+                        Key:
+                        <input type="text" value={this.state.key} onChange={this.handle_key}/>
+                    </label>
+                    <input type="submit" value="get" onClick={this.on_get}/>
+                    {store_value}
+                </div>
+
+            </div>
         );
     }
 }
--- client/src/store.js
+++ client/src/store.js
@@ -1,135 +1,37 @@
-import {extendObservable, observable, action} from "mobx";
+import {observable, action} from "mobx";
 import axios from "axios";
 
-export const tmdb_api_home = "https://api.themoviedb.org/3";
-export const tmdb_api_key = "e30bdefcb66c118c558f0793cf22e8e2";
-export const tmdb_images_prefix = "https://image.tmdb.org/t/p/original/";
-export const tmdb_small_images_prefix = "https://image.tmdb.org/t/p/w200/";
+export const keyvalue_url = "http://localhost:5555";
 
 class Store {
-    @observable movies = [];
-    @observable movie = {
-        title: null,
-        videos: []
-    };
-    @observable genres = [];
+    pairs = observable.map({});
 
-    constructor() {
-        this.page = {};
-
-        // let collection_name = "top_rated";
-        // this.append_movies(collection_name);
-        this.update_genres();
-    }
-
-    load_more = action("load_more", function (collection_name) {
-        if (typeof(store.page[collection_name]) === "undefined")
-            store.page[collection_name] = 0;
-        store.load_page(store.page[collection_name] + 1, collection_name);
-    });
-
-    load_page = action("load_page", function (val, collection_name = "top_rated") {
-        //console.log("Loading... " + collection_name);
-        this.page[collection_name] = val;
-        this.append_movies(collection_name);
-    });
-
-    get_movie = action("get_movie", function (id) {
-        let movie = null;
-        for (let i = 0; i < this.movies.length; i++) {
-            // console.log(id);
-            if (this.movies[i].id.toString() === id) {
-                this.movie = this.movies[i];
-                break;
-            }
-        }
-        this.update_movie_details(id);
-        this.update_movie_videos();
-        this.populate_movie_genres();
-        return this.movie;
-    });
-
-    update_movie_details(id) {
-        console.log(`${tmdb_api_home}/movie/${id}?api_key=${tmdb_api_key}`);
+    set_key = action("set_key", function (key, value) {
+        const url = `${keyvalue_url}/set?key=${key}&value=${value}`;
         axios
-            .get(
-                `${tmdb_api_home}/movie/${id}?api_key=${tmdb_api_key}`
-            )
+            .get(url)
             .then(res => {
-                console.log("movie: ");
                 console.log(res.data);
-                this.movie = res.data;
+                this.pairs.set(key, res.data)
             });
-    }
 
-    update_movie_videos(id) {
+        return this.pairs.get(key);
+    });
+
+    get_key = action("get_key", function (key) {
+        const url = `${keyvalue_url}/get?key=${key}`;
         axios
-            .get(
-                `${tmdb_api_home}/movie/${this.movie.id}/videos?api_key=${tmdb_api_key}`
-            )
+            .get(url)
             .then(res => {
-                // console.log(res);
-                this.movie.videos = res.data.results;
-            });
-    }
-
-    populate_movie_genres() {
-        if (!this.movie.genre_ids) return;
-        if (!this.genres) return;
-
-        //console.log(this.movie.genre_ids);
-        //console.log(this.genres);
-
-        this.movie.genres = [];
-        for (let i = 0; i < this.movie.genre_ids.length; i++) {
-            for (let j = 0; j < this.genres.length; j++) {
-                if (this.movie.genre_ids[i].toString() === this.genres[j].id.toString())
-                    this.movie.genres.push(this.genres[j]);
-                else {
-                    // console.log(this.movie.genre_ids[i] +
-                    //     " & " +
-                    //     this.genres[j].id +
-                    //     " are not equal! "
-                    // )
+                if(res.status == 200) {
+                    this.pairs.set(key, res.data)
+                } else {
+                    this.pairs.set(key, null)
                 }
-            }
-        }
-    }
-
-    append_movies(collection_name) {
-        axios
-            .get(
-                `${tmdb_api_home}/movie/${collection_name}?api_key=${tmdb_api_key}&page=${
-                    this.page[collection_name]
-                    }`
-            )
-            .then(res => {
-                // console.log(res);
-                Array.prototype.push.apply(this.movies,
-                    res.data.results.map((movie) => {
-                        if (typeof (movie["collection_name"]) === "undefined")
-                            movie["collection_name"] = [];
-                        movie["collection_name"].push(collection_name);
-                        return movie;
-                    }));
-                //console.log(this.movies);
             });
-    }
 
-    update_genres() {
-        axios
-            .get(
-                `${tmdb_api_home}/genre/movie/list?api_key=${tmdb_api_key}`
-            )
-            .then(res => {
-                //console.log(res.data);
-                Array.prototype.push.apply(this.genres,
-                    res.data.genres ? res.data.genres.map((genre) => {
-                        return genre;
-                    }) : null);
-                //console.log(this.genres);
-            });
-    }
+        return this.pairs.get(key);
+    });
 }
 
 const store = new Store();
